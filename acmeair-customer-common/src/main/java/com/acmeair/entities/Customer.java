a54c810cf8c55efbfbeba1965cd069434eb7560e
cairixian <30896945+cairixian@users.noreply.github.com>
2018-01-10 17:24:12 +0800

    Initial commit

---
+++ acmeair-customer-common/src/main/java/com/acmeair/entities/Customer.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+* Copyright (c) 2013 IBM Corp.
+* Copyright 2017 Huawei Technologies Co., Ltd
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+// this sourcecode is modified by Huawei Technologies Co., Ltd.
+package com.acmeair.entities;
+
+
+
+public interface Customer {
+
+	enum MemberShipStatus { NONE, SILVER, GOLD, PLATINUM, EXEC_PLATINUM, GRAPHITE };
+	enum PhoneType { UNKNOWN, HOME, BUSINESS, MOBILE };
+	
+	
+	String getCustomerId();
+	
+	String getUsername();
+	
+	void setUsername(String username);
+	
+	String getPassword();
+	
+	void setPassword(String password);
+	
+	MemberShipStatus getStatus();
+	
+	void setStatus(MemberShipStatus status);
+	
+	int getTotal_miles();
+	
+	int getMiles_ytd();
+	
+	String getPhoneNumber();
+
+	void setPhoneNumber(String phoneNumber);
+
+	PhoneType getPhoneNumberType();
+
+	void setPhoneNumberType(PhoneType phoneNumberType);
+
+	CustomerAddress getAddress();
+
+	void setAddress(CustomerAddress address);
+
+}
